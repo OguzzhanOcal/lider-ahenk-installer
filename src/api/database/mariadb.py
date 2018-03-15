2fccb8436d6f3644447c9db56075df62c84da856
Tuncay ÇOLAK <tuncay.colak@tubitak.gov.tr>
2018-03-15 10:28:10 +0300

    mariadb installation method added

--- src/api/database/mariadb.py
+++ src/api/database/mariadb.py
@@ -3,31 +3,42 @@
 # Author: Tuncay ÇOLAK <tuncay.colak@tubitak.gov.tr>
 
 import sys
-from api.ssh.ssh import Ssh
+# from api.ssh.ssh import Ssh
 
 class MariaDbInstaller(object):
 
     def __init__(self):
         self.connect = self.Ssh.connect(ip)
         self.run_command = self.Ssh.run_command(cmd)
-        # cmd_deb_frontend = "export DEBIAN_FRONTEND = \"noninteractive"\"
-        # cmd_pwd = "sudo debconf - set - selections << < \"mariadb-server mysql-server/root_password password $PASSWORD\""
-        # cmd_pwd_again = "sudo debconf - set - selections << < \"mariadb-server mysql-server/root_password_again password $PASSWORD\""
-        # cmd_db_install = "sudo apt - get install - y mariadb-server"
-        # cmd_create_db = "mysql -uroot -p{0} -e 'CREATE DATABASE liderdb DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci'"
-        # cmd_db_grant_privileges = "mysql -uroot -p{0} -e \"GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '{1}' WITH GRANT OPTION;\""
-        # cmd_db_replace_bind_addr = "sed -i 's/^bind-address/#&/' /etc/mysql/my.cnf"
+        self.cmd_deb_frontend = "export DEBIAN_FRONTEND = \"noninteractive\""
+        self.cmd_pwd = "sudo debconf-set-selections <<< \"mariadb-server mysql-server/root_password password {0}\""
+        self.cmd_pwd_again = "sudo debconf-set-selections <<< \"mariadb-server mysql-server/root_password_again password {0}\""
+        self.cmd_db_install = "sudo apt - get install - y mariadb-server"
+        self.cmd_create_db = "mysql -uroot -p{0} -e 'CREATE DATABASE liderdb DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci'"
+        self.cmd_db_grant_privileges = "mysql -uroot -p{0} -e \"GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '{1}' WITH GRANT OPTION;\""
+        self.cmd_db_replace_bind_addr = "sed -i 's/^bind-address/#&/' /etc/mysql/my.cnf"
 
-    def install(self, ip):
+    def install(self, ip, root_password):
         self.connect(ip)
         print("veritabanı sunucusuna bağlantı kuruldu")
-        self.run_command("mkdir /home/tcolak/db_file.txt")
-
+        self.run_command(self.cmd_deb_frontend)
+        self.run_command(self.cmd_pwd.format(root_password))
+        self.run_command(self.cmd_pwd_again.format(root_password))
+        print("veritabanı root parolaları ayarlandı.")
+        self.run_command(self.cmd_db_install)
+        # self.run_command(self.cmd_create_db.format(root_password))
+        self.run_command(self.cmd_db_replace_bind_addr)
+        print("ip: "+ip)
+        print("----->>> "+self.cmd_pwd.format(root_password))
 
 if __name__ == "__main__":
+
     ip = "127.0.0.1"
-    cmd_pwd = "secret"
-    cmd_pwd_again = "secret"
+    root_password = "secret"
+    data = {
+        'ip': ip,
+        'root_password': root_password
+    }
     db = MariaDbInstaller()
-    # db.install(ip, cmd_pwd, cmd_pwd_again)
-    db.install(ip)
+    db.install(ip, root_password)
+
