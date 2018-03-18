01b14411ab7891291c6562d94bc9009fec18900c
tncyclk <tuncay.colak@tubitak.gov.tr>
2018-03-18 13:50:35 -0400

    added ejabberd installer method to installer manager class

--- src/api/ejabberd/ejabberd.py
+++ src/api/ejabberd/ejabberd.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 # Author: İsmail BAŞARAN <ismail.basaran@tubitak.gov.tr> <basaran.ismaill@gmail.com>
+# Author: Tuncay ÇOLAK <tuncay.colak@tubitak.gov.tr>
 
 import yaml, os, io, json
 
 class EjabberInstaller(object):
 
-    def __init__(self):
+    def __init__(self, ssh_api):
+        self.ssh_api = ssh_api
         self.jabberd_template_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../conf/ejabberd_temp.yml')
         self.jabberd_out_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../dist/ejabberd.yml')
         if not os.path.exists(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../dist')):
             os.makedirs(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../dist'))
+        self.cmd_install = "echo 1 | sudo -S apt -y install ejabberd"
 
 
     def read_temp_yml_file(self):
@@ -20,22 +23,16 @@ class EjabberInstaller(object):
             except yaml.YAMLError as exc:
                 print(exc)
                 return None
+
     def write_to_yml(self,data):
-        with io.open(self.jabberd_out_path , 'w', encoding='utf8') as outfile:
+        with io.open(self.jabberd_out_path, 'w', encoding='utf8') as outfile:
             yaml.dump(data, outfile, default_flow_style=False, allow_unicode=True)
     
-    def install(self,data):
+    def install(self, data):
         yml_data = self.read_temp_yml_file()
-        #json_data = json.load(yml_data);
+        #json_data = json.load(yml_data)
         #yml_data['hosts'] = ['localhost','192.168.1.1']
         for attr in data:
             yml_data[attr] = data[attr]
-        self.write_to_yml(yml_data);
-
-
-if __name__ == "__main__":
-    data = {
-        'hosts' : ['localhost','151.10.5.12']
-    }
-    jabber = EjabberInstaller()
-    jabber.install(data)
+        self.write_to_yml(yml_data)
+        self.ssh_api.run_command(self.cmd_install)
--- src/install_manager.py
+++ src/install_manager.py
@@ -5,6 +5,7 @@
 import sys
 import time
 from api.database.mariadb import MariaDbInstaller
+from api.ejabberd.ejabberd import EjabberInstaller
 from api.ssh.ssh import Ssh
 
 class InstallManager(object):
@@ -15,6 +16,10 @@ class InstallManager(object):
     def install_mariadb(self, db_data):
         db_installer = MariaDbInstaller(self.ssh)
         db_installer.install(db_data)
+
+    def install_ejabberd(self, ejabberd_data):
+        ejabberd_installer = EjabberInstaller(self.ssh)
+        ejabberd_installer.install(ejabberd_data)
 
     def ssh_connect(self, ssh_data):
         ssh_status = self.ssh.connect(ssh_data["ip"], ssh_data["username"], ssh_data["password"])
@@ -31,14 +36,19 @@ if __name__ == "__main__":
     ssh_data = {
         "ip": "127.0.0.1",
         "username": "username",
-        "password": "secret"
+        "password": "1"
     }
 
     db_data = {
         'password': "secret",
         'db_pwd': "secret"
     }
+    ejabberd_data = {
+        'host': "liderahenk.org"
+    }
+
     im = InstallManager()
     im.ssh_connect(ssh_data)
     im.install_mariadb(db_data)
+    im.install_ejabberd(ejabberd_data)
     im.ssh_disconnect()
