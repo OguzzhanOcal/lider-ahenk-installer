71fc85a351a09db7ddefbab94eed8e93de4c7619
tncyclk <tuncay.colak@tubitak.gov.tr>
2018-03-18 13:48:50 -0400

    Wait for the command to terminate in run command method

--- src/api/ssh/ssh.py
+++ src/api/ssh/ssh.py
@@ -4,6 +4,7 @@
 
 import sys, paramiko, time
 from api.ssh.scp import SCPClient
+import select
 
 class Ssh(object):
 
@@ -32,10 +33,14 @@ class Ssh(object):
     def run_command(self, cmd):
         try:
             stdin, stdout, stderr = self.ssh.exec_command(cmd)
-            exit_status = stdout.channel.recv_exit_status()
-            # print(exit_status)
-            if exit_status == 0:
-                print("komut başarıyla çalıştırıldı...!!")
+            # Wait for the command to terminate
+            while not stdout.channel.exit_status_ready():
+                # Only print data if there is data to read in the channel
+                if stdout.channel.recv_ready():
+                    rl, wl, xl = select.select([stdout.channel], [], [], 0.0)
+                    if len(rl) > 0:
+                        # Print data from stdout
+                        print (stdout.channel.recv(1024))
         except Exception as e:
             print ("komut çalışırken hata oluştu: " + str(e))
 
